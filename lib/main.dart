65cd335fd7e46abb968e4f4ed354221428c9b906
Jirayuth <jirayuth.p@techbusiness.co.th>
2026-03-18 00:22:33 +0700

    [minor] => theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:imei_generator_flutter_web/imei.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const IMEIGen());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class IMEIGen extends StatelessWidget {
+  const IMEIGen({super.key});
 
   // This widget is the root of your application.
   @override
@@ -15,22 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'IMEI Generator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.green),
       ),
       home: const ImeiHomePage(title: 'IMEI Generator'),
     );
